b7ad1523976213b00422efd64fa78ee43e266636
Avidan Gerber <avidang@rapyd.net>
2024-10-30 11:32:47 +0200

    refactor: update UsersController to include "me" endpoint
    - Added a new GET endpoint in UsersController to retrieve the current user's information.
    - The endpoint is protected and requires a valid bearer token.
    - The endpoint is accessible at the "/me" route.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Put, Req } from "@nestjs/common";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import type { User, UserDocument } from "./users.schema";
 import { UsersService } from "./users.service";
 import { Protected } from "src/jwt/jwt.guard";
 import { ApiBearerAuth } from "@nestjs/swagger";
+import { Request } from "express";
 
 @Controller()
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Get()
-  @Protected()
-  @ApiBearerAuth()
+	@Protected()
+	@ApiBearerAuth()
 	async findAll(): Promise<User[]> {
 		return this.usersService.findAll();
 	}
 
+	@Get("me")
+	@Protected()
+	@ApiBearerAuth()
+	async me(
+		@Req() request: Request & { userId: string },
+	): Promise<UserDocument | null> {
+		return this.usersService.findOne(request.userId);
+	}
+
 	@Get(":id")
+	@Protected()
+	@ApiBearerAuth()
 	async findOne(@Param("id") id: string): Promise<UserDocument | null> {
 		return this.usersService.findOne(id);
 	}
 
 	@Put(":id")
+	@Protected()
+	@ApiBearerAuth()
 	async update(
 		@Param("id") id: string,
 		@Body() updateUserDto: UpdateUserDto,
@@ -30,6 +44,8 @@ export class UsersController {
 	}
 
 	@Delete(":id")
+	@Protected()
+	@ApiBearerAuth()
 	async remove(@Param("id") id: string): Promise<UserDocument | null> {
 		return this.usersService.remove(id);
 	}
