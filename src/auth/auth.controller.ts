90263b37f426c6b1609954bc22854de0886efb59
Avidan Gerber <avidang@rapyd.net>
2024-10-30 11:32:16 +0200

    fix: remove unused signTest endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,7 @@
-import { Controller, Get, Query, } from "@nestjs/common";
+import { Controller, } from "@nestjs/common";
 import { JwtService } from "src/jwt/jwt.service";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private jwtService: JwtService) {}
-
-	@Get("sign_test")
-	async signTest(@Query("value") value: string) {
-		return this.jwtService.jwt.sign({ value });
-	}
 }
