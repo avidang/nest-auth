839f597faedfd24925f73c3f150697185cfcef42
Avidan Gerber <avidang@rapyd.net>
2024-11-05 15:46:43 +0200

    refactor: Remove AuthModule import from AuthModule and RoleModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from "@nestjs/core";
 import { JwtModule } from "src/jwt/jwt.module";
 import { UserModule } from "src/user/user.module";
 import { GoogleProviderModule } from "./google-provider/google-provider.module";
+import { RoleModule } from "src/role/role.module";
 
 @Module({
 	imports: [
@@ -11,6 +12,7 @@ import { GoogleProviderModule } from "./google-provider/google-provider.module";
 		JwtModule,
 		GoogleProviderModule,
 		RouterModule.register([{ path: "users", module: UserModule }]),
+		RouterModule.register([{ path: "roles", module: RoleModule }]),
 	],
 	controllers: [],
 	providers: [],
