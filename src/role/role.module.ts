d63e32245bed5c54f00804ae4b51ad04dd9be83b
Avidan Gerber <avidang@rapyd.net>
2024-11-05 15:42:23 +0200

    refactor: Remove AuthModule import from RoleModule

--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,13 +1,11 @@
-import { forwardRef, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { RoleSchema } from "./role.schema";
 import { RoleController } from "./role.controller";
 import { RoleService } from "./role.service";
-import { AuthModule } from "src/auth/auth.module";
 
 @Module({
 	imports: [
-		forwardRef(() => AuthModule),
 		MongooseModule.forFeature([{ name: "Role", schema: RoleSchema }]),
 	],
 	controllers: [RoleController],
